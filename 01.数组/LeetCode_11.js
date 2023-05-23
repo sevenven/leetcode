6cb48f2a684f8ed6e0d8a3bbfba8379e09469544
苏慧芬 <suhuifen@chexiao.co>
2023-05-23 20:35:47 +0800

    update

--- 01.数组/LeetCode_11.js
+++ 01.数组/LeetCode_11.js
@@ -10,23 +10,14 @@
  */
 // 暴力破解 时间复杂度O(n^2) 空间复杂度O(1)
 var maxArea = function (height) {
-	var maxarea = 0;
-	for (var i = 0; i < height.length - 1; i++) {
-		for (var j = i + 1; j < height.length; j++) {
-			maxarea = Math.max(maxarea, (j - i) * Math.min(height[i], height[j]))
-		}
-	}
-	return maxarea;
+  let maxarea = 0;
+  for (let i = 0; i < height.length - 1; i++) {
+    for (let j = i + 1; j < height.length; j++) {
+      maxarea = Math.max(maxarea, (j - i) * Math.min(height[i], height[j]));
+    }
+  }
+  return maxarea;
 };
-// var maxArea = function (height) {
-// 	var maxarea = 0;
-// 	for (var i = 0; i < height.length - 1; i++) {
-// 		for (var j = 0; j < height.length; j++) {
-// 			maxarea = Math.max(maxarea, (j - i) * Math.min(height[i], height[j]));
-// 		}
-// 	}
-// 	return maxarea;
-// };
 
 /**
  * @param {number[]} height
@@ -34,15 +25,15 @@ var maxArea = function (height) {
  */
 // 双指针法 时间复杂度O(n) 空间复杂度O(1)
 var maxArea = function (height) {
-	var L = 0,
-		R = height.length - 1,
-		maxarea = 0;
-	while (L < R) {
-		var minHeight = height[L] < height[R] ? height[L++] : height[R--];
-		maxarea = Math.max(maxarea, (R - L + 1) * minHeight);
-	}
-	return maxarea;
-}
+  let L = 0,
+    R = height.length - 1,
+    maxarea = 0;
+  while (L < R) {
+    let minHeight = height[L] < height[R] ? height[L++] : height[R--];
+    maxarea = Math.max(maxarea, (R - L + 1) * minHeight);
+  }
+  return maxarea;
+};
 
-var maxarea = maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]) // 49
-console.log(maxarea)
+let maxarea = maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]); // 49
+console.log(maxarea);
--- 01.数组/LeetCode_15.js
+++ 01.数组/LeetCode_15.js
@@ -1,6 +1,6 @@
 // https://leetcode-cn.com/problems/3sum/
-// 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
-// 你返回所有和为 0 且不重复的三元组。
+// 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0
+// 请你返回所有和为 0 且不重复的三元组。
 // 注意：答案中不可以包含重复的三元组。
 
 /**
@@ -9,22 +9,22 @@
  */
 // 暴力破解 时间复杂度O(n^3) 空间复杂度O(1)
 var threeSum = function (nums) {
-	var result = [],
-		used = {};
-	for (var i = 0; i < nums.length - 2; i++) {
-		for (var j = i + 1; j < nums.length - 1; j++) {
-			for (var k = j + 1; k < nums.length; k++) {
-				if (nums[i] + nums[j] + nums[k] === 0) {
-					var arrStr = [nums[i], nums[j], nums[k]].sort().join('_');
-					if (!used[arrStr]) {
-						result.push([nums[i], nums[j], nums[k]]);
-						used[arrStr] = true;
-					}
-				}
-			}
-		}
-	}
-	return result;
+  let result = [],
+    exits = {};
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        if (nums[i] + nums[j] + nums[k] === 0) {
+          let arrStr = [nums[i], nums[j], nums[k]].sort().join("_");
+          if (!exits[arrStr]) {
+            result.push([nums[i], nums[j], nums[k]]);
+            exits[arrStr] = true;
+          }
+        }
+      }
+    }
+  }
+  return result;
 };
 
 /**
@@ -34,29 +34,29 @@ var threeSum = function (nums) {
 // 排序加双指针 时间复杂度O(n^2) 空间复杂度O(1)
 // 也可以先排序去重之后再做
 var threeSum = function (nums) {
-	var result = [];
-	nums.sort(function (a, b) { return a - b; });
-	for (var i = 0; i < nums.length; i++) {
-		if (nums[i] > 0) break;
-		if (i > 0 && nums[i] === nums[i - 1]) continue;
-		var L = i + 1,
-			R = nums.length - 1;
-		while (L < R) {
-			var sum = nums[i] + nums[L] + nums[R];
-			if (sum === 0) {
-				result.push([nums[i], nums[L], nums[R]]);
-				while (L < R && nums[L] === nums[L + 1]) L++;
-				while (L < R && nums[R] === nums[R - 1]) R--;
-				L++;
-				R--;
-			} else if (sum < 0) {
-				L++;
-			} else if (sum > 0) {
-				R--;
-			}
-		}
-	}
-	return result;
-}
+  let result = [];
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) break;
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let L = i + 1,
+      R = nums.length - 1;
+    while (L < R) {
+      let sum = nums[i] + nums[L] + nums[R];
+      if (sum === 0) {
+        result.push([nums[i], nums[L], nums[R]]);
+        while (L < R && nums[L] === nums[L + 1]) L++;
+        while (L < R && nums[R] === nums[R - 1]) R--;
+        L++;
+        R--;
+      } else if (sum < 0) {
+        L++;
+      } else if (sum > 0) {
+        R--;
+      }
+    }
+  }
+  return result;
+};
 
 console.log(threeSum([-1, 0, 1, 2, -1, -1, -4])); // [[-1, -1, 2], [-1, 0, 1]]
