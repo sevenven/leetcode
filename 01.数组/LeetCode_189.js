ba030b4d202aa382cd55fc2ced044098c674051d
苏慧芬 <suhuifen@chexiao.co>
2023-05-29 18:06:47 +0800

    update

--- 01.数组/LeetCode_189.js
+++ 01.数组/LeetCode_189.js
@@ -1,6 +1,5 @@
 // https://leetcode-cn.com/problems/rotate-array/
-// 给你一个数组，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。
-
+// 给你一个数组，将数组中的元素向右轮转k个位置，其中k是非负数。
 // LeetCode速度：1.环形替换法 2.反转法 3.暴力解法
 
 /**
@@ -9,12 +8,12 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 // 暴力解法
-// 时间复杂度O(k*n) 空间复杂度O(1) 
+// 时间复杂度O(k*n) 空间复杂度O(1)
 var rotate = function (nums, k) {
-  var k = k % nums.length;
-  for (var i = 0; i < k; i++) {
-    var tmp = nums[nums.length - 1];
-    for (var j = nums.length - 2; j >= 0; j--) {
+  k = k % nums.length;
+  for (let i = 0; i < k; i++) {
+    let tmp = nums[nums.length - 1];
+    for (let j = nums.length - 2; j >= 0; j--) {
       nums[j + 1] = nums[j];
     }
     nums[0] = tmp;
@@ -27,9 +26,9 @@ var rotate = function (nums, k) {
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 // 反转法
-// 时间复杂度O(n) 空间复杂度O(1) 
+// 时间复杂度O(n) 空间复杂度O(1)
 var rotate = function (nums, k) {
-  var k = k % nums.length;
+  k = k % nums.length;
   reverse(nums, 0, nums.length - 1);
   reverse(nums, 0, k - 1);
   reverse(nums, k, nums.length - 1);
@@ -37,7 +36,7 @@ var rotate = function (nums, k) {
 
 function reverse(nums, start, end) {
   while (start < end) {
-    var tmp = nums[start];
+    let tmp = nums[start];
     nums[start] = nums[end];
     nums[end] = tmp;
     start++;
@@ -51,25 +50,24 @@ function reverse(nums, start, end) {
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 // 环状替换解法
-// 时间复杂度O(n) 空间复杂度O(1) 
+// 时间复杂度O(n) 空间复杂度O(1)
 var rotate = function (nums, k) {
-  var k = k % nums.length,
-    count = 0;
-  for (var i = 0; count < nums.length; i++) {
-    var curIndex = i,
-      cur = nums[i]
+  k = k % nums.length;
+  let count = 0;
+  for (let i = 0; count < nums.length; i++) {
+    let curIndex = i,
+      cur = nums[i];
     do {
-      var moveIndex = (curIndex + k) % nums.length,
+      let moveIndex = (curIndex + k) % nums.length,
         tmp = nums[moveIndex];
       nums[moveIndex] = cur;
       cur = tmp;
       curIndex = moveIndex;
       count++;
-    } while (i !== curIndex)
+    } while (i !== curIndex);
   }
 };
 
-
-var arr = [1, 2, 3, 4, 5, 6];
+let arr = [1, 2, 3, 4, 5, 6];
 rotate(arr, 2);
 console.log(arr); // [ 5, 6, 1, 2, 3, 4 ]
--- 01.数组/LeetCode_88.js
+++ 01.数组/LeetCode_88.js
@@ -1,7 +1,7 @@
 // https://leetcode-cn.com/problems/merge-sorted-array/
-// 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
-// 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
-// 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+// 给你两个按非递减顺序排列的整数数组nums1和nums2，另有两个整数m和n，分别表示nums1和nums2中的元素数目。
+// 请你合并nums2到nums1中，使合并后的数组同样按非递减顺序排列。
+// 注意：最终，合并后数组不应由函数返回，而是存储在数组nums1中。为了应对这种情况，nums1的初始长度为 m + n，其中前m个元素表示应合并的元素，后n个元素为0，应忽略。nums2的长度为n。
 
 /**
  * @param {number[]} nums1
@@ -12,8 +12,8 @@
  */
 // 合并后排序
 var merge = function (nums1, m, nums2, n) {
-	nums1.splice(m, n, ...nums2);
-	return nums1.sort(function (a, b) { return a - b; });
+  nums1.splice(m, n, ...nums2);
+  nums1.sort((a, b) => a - b);
 };
 
 /**
@@ -26,16 +26,15 @@ var merge = function (nums1, m, nums2, n) {
 // 双指针法
 // 时间复杂度O(m+n) 空间复杂度O(1)
 var merge = function (nums1, m, nums2, n) {
-	var pos = m + n - 1;
-	m--;
-	n--;
-	while (n >= 0) {
-		nums1[pos--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
-	}
+  let pos = m + n - 1;
+  m--;
+  n--;
+  while (n >= 0) {
+    nums1[pos--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+  }
 };
 
-
-var nums1 = [1, 2, 3, 0, 0, 0];
-var nums2 = [2, 5, 6];
+let nums1 = [1, 2, 3, 0, 0, 0];
+let nums2 = [2, 5, 6];
 merge(nums1, 3, nums2, 3);
-console.log(nums1)
+console.log(nums1); // [ 1, 2, 2, 3, 5, 6 ]
