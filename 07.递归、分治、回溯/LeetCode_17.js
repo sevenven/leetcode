0764110d9fa251da9b03dda8dfbc05106b5aa939
“suhuifen” <suhuifen@chexiao.co>
2023-12-31 22:58:44 +0800

    update

--- 07.递归、分治、回溯/LeetCode_17.js
+++ 07.递归、分治、回溯/LeetCode_17.js
@@ -8,30 +8,29 @@
  */
 // 回溯解法
 var letterCombinations = function (digits) {
-	var map = {
-		'2': 'abc', 
-		'3': 'def', 
-		'4': 'ghi', 
-		'5': 'jkl', 
-		'6': 'mno', 
-		'7': 'pqrs', 
-		'8': 'tuv', 
-		'9': 'wxyz', 
-	};
-	var result = [];
-	combine('', digits, digits.length);
-	return result;
-	function combine (str, digits, len) {
-		if (len > 0 && str.length === len) {
-			result.push(str);
-			return;
-		}
-		var cur = digits.length > 0 ? map[digits.slice(0, 1)] : '';
-		for (c of cur) {
-			combine(str + c, digits.slice(1), len);
-		}
-	}
+  var map = {
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  };
+  var result = [];
+  combine("", digits, digits.length);
+  return result;
+  function combine(str, digits, len) {
+    if (len > 0 && str.length === len) {
+      result.push(str);
+      return;
+    }
+    var cur = digits.length > 0 ? map[digits.slice(0, 1)] : "";
+    for (c of cur) {
+      combine(str + c, digits.slice(1), len);
+    }
+  }
 };
 
 console.log(letterCombinations("23")); // ['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf']
-
